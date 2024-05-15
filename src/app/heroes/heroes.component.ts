d65ba9c32f4302095a501592694621df0952abe1
Gian Mehring <dev.gian.trabajos@gmail.com>
2024-05-15 12:14:44 -0300

    Lección 2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,5 +9,12 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent {
-  hero: Hero = { id: 1, name: "Windsorm" }
+  // hero: Hero = { id: 1, name: "Windsorm" }
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
